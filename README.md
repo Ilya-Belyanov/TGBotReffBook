572a5e0c2d9ee52e4d7dd71d41b1f8bbb7ad2cfe
Ilya-Belyanov <belyanov.99@mail.ru>
2022-10-26 22:42:48 +0300

    Create Docker build

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # TGBotReffBook
 ***
 
-Configure
+Configure for Docker build
 =====================
-1. Create file **config.py** in the data folder.
-2. Add var **TOKEN = "YOU_BOT_TOKER"** to config.py.
+1. Create secret token from **token.txt** file (create it and write your bot token).
+2. Build image by **Dockerfile**.
+3. Run service **docker service create --name poly-schedule-bot --secret token 031983/poly-schedule-bot**
--- app/bot.py
+++ app/bot.py
@@ -4,11 +4,20 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
+<<<<<<< HEAD
 import config
 
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w')
+=======
+logging.basicConfig(level=logging.INFO)
+>>>>>>> 44754cd... Create Docker build
 
-bot_object = Bot(token=config.TOKEN)
+try:
+    token = open('/run/secrets/token', 'r').readline()
+    bot_object = Bot(token=token)
+except IOError:
+    exit(-1)
+
 dispatcher = Dispatcher(bot_object, storage=MemoryStorage())
 dispatcher.middleware.setup(LoggingMiddleware())
 
