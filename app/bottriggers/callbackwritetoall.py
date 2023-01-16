fc3e4de3041cf19ff03bcdf75d4304b5f067e78d
Ilya-Belyanov <belyanov.99@mail.ru>
2023-01-16 18:43:44 +0300

    sending all type message to users

--- app/bottriggers/callbackwritetoall.py
+++ app/bottriggers/callbackwritetoall.py
@@ -8,15 +8,20 @@ from app.data.keyspace import DatabaseColumnsUser
 
 
 # Обработка введенного текста
-@dispatcher.message_handler(content_types=types.ContentType.TEXT, state=StateMachine.WRITE_TO_ALL)
+@dispatcher.message_handler(content_types=types.ContentType.ANY, state=StateMachine.WRITE_TO_ALL)
 async def process_callback_write_to_all(msg: types.Message):
-    txt = msg.text
     users = await all_users()
     for user in users:
         try:
-            await bot_object.send_message(user[0], txt)
+            if msg.text is not None:
+                await bot_object.send_message(user[0], msg.text)
+            if msg.photo is not None and len(msg.photo) > 0:
+                await bot_object.send_photo(user[0], photo=msg.photo[-1].file_id,
+                                            caption=msg.caption if msg.caption else "")
+            if msg.document is not None:
+                await bot_object.send_document(user[0], document=msg.document.file_id,
+                                               caption=msg.caption if msg.caption else "")
             await save_int_for_user(user[0], DatabaseColumnsUser.ACTIVE, 1)
         except Exception as e:
             await save_int_for_user(user[0], DatabaseColumnsUser.ACTIVE, 0)
     await StateMachine.MAIN_STATE.set()
-
